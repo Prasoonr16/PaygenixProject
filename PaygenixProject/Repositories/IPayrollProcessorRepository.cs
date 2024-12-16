7489921d5c7fc405e54074e155dc8752b8490c65
Abhishek0712-ctrl <abhishekg9593@gmail.com>
2024-12-16 13:03:25 +0530

    updated the payroll processor controller

--- PaygenixProject/Repositories/IPayrollProcessorRepository.cs
+++ PaygenixProject/Repositories/IPayrollProcessorRepository.cs
@@ -7,9 +7,12 @@ namespace NewPayGenixAPI.Repositories
     {
 
         Task<IEnumerable<Payroll>> GetPayrollByEmployeeIdAsync(int employeeId);
+        Task<List<PayrollDTO>> FetchPayrollsByPeriodAsync(DateTime startPeriod, DateTime endPeriod);
         //Task VerifyPayrollAsync(int employeeId);
-        Task<Payroll> ProcessPayrollAsync(int employeeId, PayrollDTO payrollDto);
+        //Task<Payroll> ProcessPayrollAsync(int employeeId, PayrollDTO payrollDto);
         Task<bool> VerifyPayrollAsync(int payrollId);
+        Task<PayrollDTO> ProcessPayrollByIdAsync(int payrollId);
         Task<(EmployeeDTO, decimal)> GetEmployeeByPayrollIdAsync(int payrollId);
+        //Task<List<Payroll>> ProcessPayrollsAsync(DateTime startPeriod, DateTime endPeriod);
     }
 }
